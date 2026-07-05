f362a021b5bea7a9e0378b91c7c96582a4a1a0e8
Sheikh Mohammad <sheikhmohammadahmed07@gmail.com>
2026-07-05 21:32:28 +0500

    Add class 01 code

---
+++ backend/class-01/app.js
@@ -0,0 +1,17 @@
+// console.log("Hello World");
+
+// import fs from "fs";
+
+// const createFile = () => {
+//   fs.writeFileSync("./name.txt", "Sheikh Mohammad");
+//   console.log("File created");
+// };
+
+// const readFile = () => {
+//   const content = fs.readFileSync("./name.txt", "utf-8");
+//   console.log("File readed");
+//   console.log("File Content:", content);
+// };
+
+// createFile();
+// readFile();
