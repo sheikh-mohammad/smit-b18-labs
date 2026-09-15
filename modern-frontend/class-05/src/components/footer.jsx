2ec3f52f83ad7bc916d4cb9830e72a0f37eedb88
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2026-09-15 15:46:16 +0500

    Add half code for class-05

---
+++ modern-frontend/class-05/src/components/footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = (props) => {
+  return (
+    <div>
+      <h1>USERNAME: {props.fullName}</h1>
+      <h1>Footer</h1>
+    </div>
+  );
+};
+
+export default Footer;
