936891a2c6b008b6777f27848e176609266c1714
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2026-09-18 17:40:32 +0500

    Listing done in class 05

--- modern-frontend/class-05/src/components/navbar.jsx
+++ modern-frontend/class-05/src/components/navbar.jsx
@@ -26,8 +26,15 @@ const Navbar = (props) => {
     getSize(size);
   };
 
+  const cities = ["Karachi", "Lahore", "Islamabad"];
+
   return (
     <div>
+      <ul>
+        {cities.map((city, index) => {
+          return <li key={index}>{index + 1}. {city}</li>
+        })}
+      </ul>
       <h1>Navbar</h1>
       <h2>Username: {fullName}</h2>
       <h2>Age: {age}</h2>
